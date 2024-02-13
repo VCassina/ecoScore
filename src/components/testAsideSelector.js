b7c5d10558bee48d5dff164ffe69229aa511c4b7
VCassina <victorcassina@gmail.com>
2024-02-13 16:28:30 +0100

    Le site est tout cassé.

--- src/components/testAsideSelector.js
+++ src/components/testAsideSelector.js
@@ -3,21 +3,23 @@ import useIsMobile from "../helpers/useIsMobile.js";
 
 function TestAsideSelector({ data, currentQuestionIndex, setResultScreen }) {
   const isMobile = useIsMobile();
+  console.log(data.subjects.length);
 
   return (
     <aside>
       <ul
         className={`${
           isMobile === "mobile" ? "" : "flex-col gap-8"
-        } flex flex-row justify-between`}
+        } flex flex-row justify-between w-80`}
       >
+      
         {Array.from({ length: data.subjects.length }, (_, i) => (
           <li
             key={i}
             className={`h-${isMobile === "mobile" ? "2" : "6"} w-${
               isMobile === "mobile" ? "6" : "2"
-            } ${i === currentQuestionIndex - 1 ? "selected" : "bg-lagon"}`}
-          ></li>
+            } ${i === currentQuestionIndex - 1 ? "selected" : "bg-lagon"} block`}
+          >&nbsp;</li>
           
         ))}
       </ul>
